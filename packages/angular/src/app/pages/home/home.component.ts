da200dc152ddeb1c292edb9f4d17541878c8f9df
Harry Shuttleworth <hshuttleworth0@gmail.com>
2021-10-09 17:18:19 +0100

    Add form click throughs

--- packages/angular/src/app/pages/home/home.component.ts
+++ packages/angular/src/app/pages/home/home.component.ts
@@ -9,7 +9,33 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  showInitializeMessage: boolean = true;
+  showBuildParameters: boolean = false;
+  showCallFunction: boolean = false;
+  showSuccessCard: boolean = false;
+
   ngOnInit(): void {
   }
 
+  toggleBuildParameters() {
+    this.showBuildParameters = !this.showBuildParameters;
+    this.showInitializeMessage = !this.showInitializeMessage;
+  }
+
+  toggleCallFunction() {
+    this.showBuildParameters = !this.showBuildParameters;
+    this.showCallFunction = !this.showCallFunction;
+  }
+
+  toggleSuccessCard() {;
+    this.showCallFunction = !this.showCallFunction;
+    this.showSuccessCard = !this.showSuccessCard;
+  }
+
+  backToStart() {
+    this.showInitializeMessage = true;
+    this.showBuildParameters = false;
+    this.showCallFunction = false;
+    this.showSuccessCard = false;
+  }
 }
