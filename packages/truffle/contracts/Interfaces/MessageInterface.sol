34e595113c8a16a937ff1820150b924b2800939b
Karl Schmidt <karthedew@gmail.com>
2021-10-15 17:24:36 -0700

    updating contracts, making them upgradeable

---
+++ packages/truffle/contracts/Interfaces/MessageInterface.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/vendor/BufferChainlink.sol";
+
+
+interface MessageInterface {
+    struct Message {
+        uint64 id;
+        string method;
+        address callback;
+        uint32 amount;
+        address destination;
+        BufferChainlink.buffer buf;
+    }
+}
