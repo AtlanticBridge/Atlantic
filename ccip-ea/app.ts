f33e9880da7094e8bd84f94983a174748578c7c0
Karl Schmidt <karthedew@gmail.com>
2021-11-10 21:40:04 -0800

    added callback integration test and callback event catch

--- ccip-ea/app.ts
+++ ccip-ea/app.ts
@@ -5,13 +5,15 @@ import { routes } from "./routes/routes";
 import Web3 from "web3";
 import { config } from "dotenv";
 import { callbackEth } from "./callback/eth.callback";
-import receiveMessageAbi from "./abi/ReceiveMessage.json";
+// import receiveMessageAbi from "./abi/ReceiveMessage.json";
+import AtlanticReceiver from "../packages/angular/src/app/artifacts/abis/AtlanticReceiverV1.json"
+import jsonString from "../packages/truffle/outputs/3_integration_test_callback.json"
 
 config();
 const web3BscProviderWs = new Web3(new Web3.providers.WebsocketProvider(process.env.BSC_TESTNET_MORALIS_WS as string));
 const wsAtlanticReceiverContract = new web3BscProviderWs.eth.Contract(
-  receiveMessageAbi.abi as any,
-  '0xEaed3B434d0FFf6D6d7AA80D72a3B47dD86A3617'
+  AtlanticReceiver.abi as any,
+  jsonString.address
 );
 
 const startServer = async () => {
@@ -36,8 +38,18 @@ const init = async () => {
     })
     .on('changed', (changed: any) => console.log('The changed: ', changed))
     .on('error', (err: any) => console.error('The error: ', err))
-    .on('connected', (str: any) => console.log(`Connected to Smart Contract | ${str} | on Binance Smart Chain Testnet`));
-};
+    .on('connected', (str: any) => console.log(`Connected to Smart Contract | ${jsonString.address} | on Binance Smart Chain Testnet`));
+
+    wsAtlanticReceiverContract.events.CallbackMessage()
+    .on('data', (event: any) => {
+      console.log('The event: ', event);
+      // callbackEth(event);
+      // callChainlinkNode("")
+    })
+    .on('changed', (changed: any) => console.log('The changed: ', changed))
+    .on('error', (err: any) => console.error('The error: ', err))
+    .on('connected', (str: any) => console.log(`Connected to Smart Contract | ${jsonString.address} | on Binance Smart Chain Testnet. \n//** Watching CallbackMessage **//`));
+  };
 
 init();
 startServer();
--- ccip-ea/callback/eth.callback.ts
+++ ccip-ea/callback/eth.callback.ts
@@ -5,17 +5,17 @@ import receiveMessageFile from "../abi/ReceiveMessage.json";
 config();
 
 function callbackEth(event: any) {
-    const method = event.returnValues._method
-    const callback = event.returnValues._callback
-    const amount = event.returnValues._amount
-    const destination = event.returnValues._destination
+    const method = event.returnValues._method;
+    const callback = event.returnValues._callback;
+    const amount = event.returnValues._amount;
+    const destination = event.returnValues._destination;
 
     const web3Kovan = new Web3(process.env.KOVAN_HTTP_INFURA as string);
     const receiveMessage = new web3Kovan.eth.Contract(
         receiveMessageFile.abi as any,
         destination
-    )
-    console.log('The results (_method): ', event.returnValues._method)
+    );
+    console.log('The results (_method): ', event.returnValues._method);
     const { address: admin } = web3Kovan.eth.accounts.wallet.add(process.env.KOVAN_PRIVATE_KEY as string);
     /**
      * We want to take the event parameter information (bytes) and pass that back to ethereum.
@@ -31,19 +31,19 @@ export { callbackEth };
 /**
  * --- Only Show This at Chainlink Hackathon ---
  * USER CONTRACT <=> ATLANTIC CONTRACT (Blockchain X) <==> BRIDGE <==> ATLANTIC CONTRACT (Blockchain Y) <==> USER CONTRACT
- * 
+ *
  * Third Party => own Messages => charge small fee other users use Message
- * 
+ *
  * Road Map
- * [1] Messages Market 
+ * [1] Messages Market
  * [2] Block Explorer for Messages
  * [3] Charge goes to Message owners
  * [4] Data Analytics Platform <==> The Graph
- * 
+ *
  * Streams of Revenue
  * [1] Message Creating Fee
  * [2] Building out Atlantic Products on Top of Message Protocol
- * 
+ *
  * NOTES:
  * [1] Extract bytes parameter information
  * [2] Message Market
@@ -51,11 +51,11 @@ export { callbackEth };
  * [3] NFT - own royality share of that Message.
  * [4] Add flag on Message for *private* or *public*
  * [5] How could someone "attack" the message / message protocol.
- * 
+ *
  * IDEAS:
  * [1] Private Messages for Message Owners
  * [2] Smart Contract Audit
  * [3] Token Sale before Launch
  * [4] Deploy Contracts?
- * 
+ *
  */
