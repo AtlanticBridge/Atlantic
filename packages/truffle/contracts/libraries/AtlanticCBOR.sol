d27859e3a5418e9b0d7ab634042658eb2616a45d
Harry Shuttleworth <hshuttleworth0@gmail.com>
2021-11-14 17:05:23 +0000

    Add basic buffer decoder

--- packages/truffle/contracts/libraries/AtlanticCBOR.sol
+++ packages/truffle/contracts/libraries/AtlanticCBOR.sol
@@ -1,12 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0;
 
-function decodeBuffer() {
+contract AtlanticCBOR {
 
-}
+    string[] parameters;
+
+    function decodeBuffer(bytes calldata _params) public {
+
+        bytes calldata working_object = _params;
+
+        if (uint8(working_object[0]) == 6) {
+            uint8 input_length = uint8(working_object[1]);
+            bytes calldata input = bytes(working_object[1: 2 + input_length]);
+            parameters.push(decodeString(input));
+            working_object = working_object[3 + input_length:];
+        } else if (uint8(working_object[0]) == 7 && uint8(working_object[1]) == 8) {
+            
+        }
+    }
+
+    function decodeString(bytes memory _string) private pure returns(string memory) {
+        return string(abi.encodePacked(_string));
+    }
 
 // string
 // bytes
 // int
 // uint
 // string array
+
+}
