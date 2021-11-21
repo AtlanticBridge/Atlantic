4cc96c0d1fdd5c938c1abd66e0ceda897985bfee
Harry Shuttleworth <hshuttleworth0@gmail.com>
2021-11-21 18:01:20 +0000

    Complete processing of working object

--- packages/truffle/contracts/libraries/AtlanticCBOR.sol
+++ packages/truffle/contracts/libraries/AtlanticCBOR.sol
@@ -5,87 +5,59 @@ contract AtlanticCBOR {
 
     string[] parameters;
 
-    function decodeBuffer(bytes calldata _params) public {
-
-        bytes calldata working_object = _params;
-        uint8 input_length;
-        uint256 hex_length;
-        int8 value;
-        bool is_parameter = true;
-
-        // first_digit
-        // input_length -- input_signifier
-        // extra_length
-        // hex_length
-
+    function decodeBuffer(bytes calldata working_object) public {
         
         while (working_object.length > 0) {
             uint8 first_digit = uint8(working_object[0]);
             if (first_digit == 6) {
                 working_object = processDecoding(working_object, 1, 0, true);
-                // input_length = uint8(working_object[1]);
-                // bytes calldata input = bytes(working_object[2: 2 + input_length]);
-                // parameters.push(decodeString(input));
-                // working_object = working_object[3 + input_length:];
             } else if (first_digit == 7) {
                 if (uint8(working_object[1]) < 8) {
-                    input_length = 16 + uint8(working_object[1]);
-                    hex_length = 0;
+                    working_object = processDecoding(working_object, 1, 16, true);
                 } else {
-                    hex_length = 2 ** (uint8(working_object[1]) - 7);
-                    // input_length = uint8(working_object[2: 2 + hex_length]);
+                    working_object = processDecoding(working_object, 2, 0, true);
                 }
-                bytes calldata input = bytes(working_object[2 + hex_length: 2 + hex_length + input_length]);
-                parameters.push(decodeString(input));
-                working_object = working_object[3 + hex_length + input_length:];
             } else if (first_digit == 0) {
-                input_length = 1;
-                working_object = working_object[1 + input_length:];
-                value = int8(uint8(working_object[1]));
+                working_object = processDecoding(working_object, 1, 0 , false);
             } else if (first_digit == 1) {
                 if (uint8(working_object[1]) < 8) {
-                    value = 16 + int8(uint8(working_object[1]));
+                    working_object = processDecoding(working_object, 1, 16 , false);
                 } else {
-                    hex_length =  2 ** (uint8(working_object[1]) - 7);
+                    working_object = processDecoding(working_object, 2, 0 , false);
                 } 
-
             } else if (first_digit == 2) {
-                input_length = 1;
-                working_object = working_object[1 + input_length:];
-                value = -int8(uint8(working_object[1])) - 1;
+                working_object = processDecoding(working_object, 1, -1, false);
             } else if (first_digit == 3) {
                 if (uint8(working_object[1]) < 8) {
-                    value = -16 - int8(uint8(working_object[1])) - 1;
+                    working_object = processDecoding(working_object, 1, -17, false);
                 } else {
-                    hex_length =  2 ** (uint8(working_object[1]) - 7);
+                    working_object = processDecoding(working_object, 2, -1, false);
                 }
             }
         }
     }
 
-    function processDecoding(bytes calldata working_object, uint8 input_signifiers, uint8 extra_length, bool _string) pure public returns (bytes calldata new_working_object) {
+    function processDecoding(bytes calldata working_object, uint8 input_signifiers, int8 extra_length, bool _string) pure public returns (bytes calldata new_working_object) {
+
         uint256 hex_length = 0;
+        int256 input_length = 0;
+
         if ((uint8(working_object[1]) - 7) > 0) {
             hex_length = 2 ** (uint8(working_object[1]) - 7);
         }
-        uint8 input_length = uint8(working_object[input_signifiers]) + extra_length + uint8(hex_length);
-        bytes memory input = bytes(working_object[1 + input_signifiers: 1 + input_signifiers + input_length]);
+        input_length = int256(uint256(bytes32(working_object[input_signifiers: input_signifiers + uint256(hex_length)])));
         if (_string) {
-            decodeString(input);
+            bytes memory input = bytes(working_object[1 + input_signifiers: 1 + uint256(input_signifiers) + uint256(input_length)]);
+            string memory value = decodeString(input);
         } else {
-
+            int256 value = input_length;
+            input_length = int256(hex_length);
         }
-        return working_object[1 + 1 + input_signifiers + input_length:];
+        return working_object[1 + 1 + uint256(input_signifiers) + uint256(input_length):];
     }
 
     function decodeString(bytes memory _string) private pure returns(string memory) {
         return string(abi.encodePacked(_string));
     }
-
-// string
-// bytes
-// int
-// uint
-// string array
 
 }
